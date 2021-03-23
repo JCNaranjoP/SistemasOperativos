bff4b7b6a90743ce49c615b80f0699752fae8ab5
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-03-23 02:59:49 -0500

    solucion proyecto 1 p3.c

---
+++ Unidad 2/Proyecto1/p3.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char *argv[]) {
+    pid_t pid_hijo1, pid_hijo2;
+    int status1,status2;
+    pid_hijo1 = fork();
+    switch(pid_hijo1) {
+        case -1: /* Error */
+            printf("Error al crear el proceso");
+        return -1;
+        case 0: /* Código ejecutado por el hijo1 */
+           execv("./p1",argv);
+        break;
+        default: /* Código ejecutado por el padre */
+            pid_hijo2 = fork();
+            switch(pid_hijo2) {
+                case -1: /* Error */
+                    printf("Error al crear el proceso");
+                return -1;
+                case 0: /* Código ejecutado por el hijo2 */
+                    execv("./p2",argv);
+                break;
+                default: /* Código ejecutado por el padre */
+                    /* Esperamos al primer hijo */
+                    waitpid(pid_hijo1, &status1, 0);
+                    /* Esperamos al segundo hijo */
+                    waitpid(pid_hijo2, &status2, 0);
+            }
+    }
+     /* Esta línea será ejecutada por ambos procesos, pero en
+        en diferente contexto (el de cada proceso) */
+        if(pid_hijo1==0) printf("Soy el hijo1 terminando\n");
+         else if(pid_hijo2==0) printf("Soy el hijo2 terminando\n");
+         else printf("Soy el padre terminando\n");
+         
+     exit(EXIT_SUCCESS);
+ }
