733709a0e7c11d194532f9df74f5262f059e4247
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-04-26 23:39:08 -0500

    creo el main y las variables a utilizar

--- Unidad 3/Proyecto 1/proyecto1.c
+++ Unidad 3/Proyecto 1/proyecto1.c
@@ -2,3 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    pid_t pid_hijo;
+    int status;
+    int pfdPadre[2];
+    int pfdHijo[2];
+    int MSGSIZE = 100;
+    char buf[MSGSIZE];
+    
+    exit(EXIT_SUCCESS);
+}
