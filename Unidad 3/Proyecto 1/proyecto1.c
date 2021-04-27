1eaddc9b5928ee75025791f103cce1bc737546bb
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-04-26 23:41:16 -0500

    creo el proceso hijo

--- Unidad 3/Proyecto 1/proyecto1.c
+++ Unidad 3/Proyecto 1/proyecto1.c
@@ -12,6 +12,19 @@ int main(int argc, char *argv[]) {
     char buf[MSGSIZE];
     pipe(pfdPadre);
     pipe(pfdHijo);
-    
+    pid_hijo = fork();
+    switch(pid_hijo) {
+        case -1: /* Error */
+            printf("Error al crear el proceso");
+            return -1;
+        case 0: /* Código ejecutado por el hijo */
+            
+            break;
+        default: /* Código ejecutado por el padre */
+            
+            waitpid(pid_hijo, &status, 0);
+    }
+    /* Esta línea será ejecutada por ambos procesos, pero en
+        en diferente contexto (el de cada proceso) */
     exit(EXIT_SUCCESS);
 }
