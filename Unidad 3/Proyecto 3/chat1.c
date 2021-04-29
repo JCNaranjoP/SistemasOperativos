94889c2018814162b35038d60f7d0276f1a0d0c6
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-04-29 15:11:16 -0500

    Creo el archivo con los include

---
+++ Unidad 3/Proyecto 3/chat1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include <pthread.h>
+
