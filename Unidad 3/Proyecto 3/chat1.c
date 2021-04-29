ea78f23c6072efa5e32dbdfd675664d6230d8b50
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-04-29 15:12:32 -0500

    init control mechanism

--- Unidad 3/Proyecto 3/chat1.c
+++ Unidad 3/Proyecto 3/chat1.c
@@ -10,3 +10,23 @@
 #include <semaphore.h>
 #include <pthread.h>
 
+#define SH_SIZE 16
+
+int shared_fd = -1;
+char mem[20];
+int32_t *counter = NULL;
+sem_t *semaphore = NULL;
+sem_t *semaphore1 = NULL;
+
+void init_control_mechanism() {
+	semaphore = sem_open("sem0", O_CREAT, 0666, 0);
+	if (semaphore == SEM_FAILED) {
+		perror("Opening the semaphore failed: ");
+		exit(EXIT_FAILURE);
+	}
+	semaphore1 = sem_open("sem1", O_CREAT, 0666, 0);
+	if (semaphore == SEM_FAILED) {
+		perror("Opening the semaphore failed: ");
+		exit(EXIT_FAILURE);
+	}
+}
